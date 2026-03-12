f64fa42e17be0c5b6977b97e20ce8b4f01dbf559
MrSufferer <tkien2703@gmail.com>
2026-03-12 09:33:38 +0700

    add unit test

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.js";
+
+const person = {
+    isActive: true,
+    age: 32,
+};
+
+describe("person", () => {
+    test("person is defined", () => {
+        expect(person).toBeDefined();
+    });
+
+    test("is active", () => {
+        expect(person.isActive).toBeTruthy();
+    });
+});
+
+describe("getAPIKey", () => {
+    test("returns API key", () => {
+        const headers = {
+            authorization: "ApiKey1234567890",
+        };
+        expect(getAPIKey(headers)).toBe("1234567890");
+    });
+});
