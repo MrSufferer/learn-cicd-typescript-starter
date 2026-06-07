bdfea2f3510e105a66c8249bfe9ee916c2d4d837
MrSufferer <tkien2703@gmail.com>
2026-06-08 00:17:04 +0700

    use crypto.randomBytes() instead of crypto.pseudoRandomBytes()

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 import pluginSecurity from "eslint-plugin-security";
 
-
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
