0e919e1e206fa92785255d8d7337f4d6a4f18d09
Kyler <107874128+MrSufferer@users.noreply.github.com>
2026-06-08 10:44:14 +0700

    add CI checks (#2)
    * change readme
    
    * add ci.yml
    
    * change ci.yml
    
    * add unit test
    
    * fix auth.ts test
    
    * add test coverage flag to CI
    
    * add badge.svg
    
    * add format check
    
    * format:write
    
    * add lint in ci.yml
    
    * remove unused code
    
    * add eslint security plugin
    
    * use crypto.randomBytes() instead of crypto.pseudoRandomBytes()
    
    * add CD

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,25 +2,25 @@ import { describe, expect, test } from "vitest";
 import { getAPIKey } from "../api/auth.js";
 
 const person = {
-    isActive: true,
-    age: 32,
+  isActive: true,
+  age: 32,
 };
 
 describe("person", () => {
-    test("person is defined", () => {
-        expect(person).toBeDefined();
-    });
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
 
-    test("is active", () => {
-        expect(person.isActive).toBeTruthy();
-    });
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
 });
 
 describe("getAPIKey", () => {
-    test("returns API key", () => {
-        const headers = {
-            authorization: "ApiKey 1234567890",
-        };
-        expect(getAPIKey(headers)).toBe("1234567890");
-    });
+  test("returns API key", () => {
+    const headers = {
+      authorization: "ApiKey 1234567890",
+    };
+    expect(getAPIKey(headers)).toBe("1234567890");
+  });
 });
